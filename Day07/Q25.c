7610494eadf0616f02bc532f7ce86f4cf45d9e8c
Rishabh Sonkar <rishabh6714@gmail.com>
2026-06-05 13:03:00 +0530

    Day01 uplodaded.

--- Day07/Q25.c
+++ Day07/Q25.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int factorial(int n) 
 {
-    if(n == 0 || n == 1)
-        return 1;
-    else
-        return n * factorial(n - 1);
+if(n == 0 || n == 1)
+return 1;
+else
+return n * factorial(n - 1);
 }
-
 int main()
 {
-    int num = 5;
-    printf("Factorial of %d = %d", num, factorial(num));
-    return 0;
+int num = 5;
+printf("Factorial of %d = %d", num, factorial(num));
+return 0;
 }
